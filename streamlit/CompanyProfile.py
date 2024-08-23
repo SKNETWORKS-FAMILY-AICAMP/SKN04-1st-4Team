e519a70362bc5c55a9cfef92934879637162606b
Jungyunn <34234513+Jungyunn@users.noreply.github.com>
2024-08-23 13:51:03 +0900

    feat: company page fixed

--- streamlit/CompanyProfile.py
+++ streamlit/CompanyProfile.py
@@ -3,28 +3,33 @@ import time
 import datetime
 
 # 이미지 파일 경로 설정
-image_path = "C:\\Users\\Playdata\\Documents\\git\\Project_4\\SKN04-1st-4Team\\logo.png"  # 이미지 파일의 경로
+image_path = "../logo.png"  # 이미지 파일의 경로
 
 # 이미지 표시
 st.image(image_path ,use_column_width=True)
 
-st.title('회사소개')
-st.text("열심히 자동차를 만들고 전국 자동차 수를 파악하는 기업")
+st.markdown("<h1 style='text-align: center;'>엔코아 자동차</h1>", unsafe_allow_html=True)
+st.markdown("<h5 style='text-align: center;'>전국에서 최대 30%의 지분을 차지하는 엔코아 자동차! </h5>", unsafe_allow_html=True)
 
 
-col1, col2= st.columns([0.3, 0.7])
+st.text("")
+st.text("")
+st.text("")
 
-with col1:
-    st.header('이름')
-    st.subheader('오정연')
-    st.subheader('김현재')
-    st.subheader('권오셈')
-    st.subheader('오종수')
 
-with col2:
-    st.header('역할')
-    st.subheader('팀장 : 데이터 크롤링, DB')
-    st.subheader('팀원 : 데이터 크롤링, ㄴㄴㄴㄴ')
-    st.subheader('팀원 : 데이터 크롤링, ㄴㄴㄴㄴ')
-    st.subheader('팀원 : 데이터 크롤링, ㄴㄴㄴㄴ')
+# 창립멤버 섹션
+st.markdown("<h2 style='text-align: center; color: #2B6465;'>창립멤버</h2>", unsafe_allow_html=True)
+
+# 멤버 목록
+members = ["오정연", "김현재", "권오셈", "오종수"]
+
+# 창립멤버를 카드 형식으로 표시
+for member in members:
+    st.markdown(f"""
+        <div style='display: flex; justify-content: center; align-items: center; margin-top: 10px;'>
+            <div style='background-color: #f9f9f9; padding: 8px; border-radius: 10px; box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.1); width: 50%; text-align: center;'>
+                <h3 style='margin: 0; color: #333;'>{member}</h3>
+            </div>
+        </div>
+    """, unsafe_allow_html=True)
 
